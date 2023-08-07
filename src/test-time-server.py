46f504345e36b52a2c0479ebbb19bf8fb382471e
Melody Gill <melodywgill@gmail.com>
2023-08-07 15:15:44 -0700

    Add files to test getting server time

---
+++ src/test-time-server.py
@@ -0,0 +1,53 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText 2023 Kristin Ebuengan
+# SPDX-FileCopyrightText 2023 Melody Gill
+# SPDX-FileCopyrightText 2023 Gabriel Marcano
+
+"""
+Writes current time as a tuple to serial
+"""
+
+# from datetime import datetime
+import time
+import serial
+
+
+INTERVAL = 0.05  # in seconds
+DELAY = 0.05  # in seconds
+
+def constant_time(port):
+    ser = serial.Serial(port) # open serial port
+    ser.baudrate = 115200
+
+    startTime = time.monotonic()
+    endTime = startTime + INTERVAL
+    now = time.monotonic()
+
+    while now < endTime:   # runs for the specified interval
+        currTime = time.time()
+        currSeconds = int(currTime)
+        currHundredths = int((currTime - currSeconds) * 100)
+
+
+        # currTime = datetime.utcnow()
+        # currWeekday = time.gmtime().tm_wday
+        # currMillisec = int(currTime.microsecond/10000)
+        # currTuple = (
+        #     currTime.year,
+        #     currTime.month,
+        #     currTime.day,
+        #     currWeekday,
+        #     currTime.hour,
+        #     currTime.minute,
+        #     currTime.second,
+        #     currMillisec
+        # )
+        toWrite = bytearray(f"{currSeconds} {currHundredths}\n", 'utf-8')
+        ser.write(toWrite)
+
+        time.sleep(DELAY)
+        now = time.monotonic()
+
+    ser.close() # close serial port
+
+constant_time("/dev/ttyUSB0")
---
+++ src/test-time.c
@@ -0,0 +1,74 @@
+// test time synchronization
+
+#include <rtc.h>
+
+#include <cli.h>
+#include <uart.h>
+#include <syscalls.h>
+
+#include "am_mcu_apollo.h"
+#include "am_bsp.h"
+#include "am_util.h"
+
+#include <sys/time.h>
+
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+#include <inttypes.h>
+
+struct uart uart;
+struct am1815 rtc;
+struct spi_bus spi;
+struct spi_device rtc_spi;
+struct cli cli;
+
+__attribute__((constructor))
+static void redboard_init(void)
+{
+	// Prepare MCU by init-ing clock, cache, and power level operation
+	am_hal_clkgen_control(AM_HAL_CLKGEN_CONTROL_SYSCLK_MAX, 0);
+	am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
+	am_hal_cachectrl_enable();
+	am_bsp_low_power_init();
+	am_hal_sysctrl_fpu_enable();
+	am_hal_sysctrl_fpu_stacking_enable(true);
+
+	spi_bus_init(&spi, 0);
+	spi_bus_enable(&spi);
+	spi_bus_init_device(&spi, &rtc_spi, SPI_CS_3, 2000000u);
+	am1815_init(&rtc, &rtc_spi);
+
+	// cli_init(&cli);
+	uart_init(&uart, UART_INST0);
+	syscalls_uart_init(&uart);
+	syscalls_rtc_init(&rtc);
+
+	// After init is done, enable interrupts
+	am_hal_interrupt_master_enable();
+}
+
+__attribute__((destructor))
+static void redboard_shutdown(void)
+{
+	// cli_destroy(&cli);
+}
+
+int main(void)
+{
+    // test am1815_write_time
+    struct timeval curr_time = am1815_read_time(&rtc);
+    am_util_stdio_printf("Time before change: %llu seconds, %ld microseconds\r\n", curr_time.tv_sec, curr_time.tv_usec);
+
+    struct timeval new_time = {.tv_sec = 1000003000, .tv_usec = 40};
+    am1815_write_time(&rtc, &new_time);
+    curr_time = am1815_read_time(&rtc);
+    am_util_stdio_printf("Time after change: %llu seconds, %ld microseconds\r\n", curr_time.tv_sec, curr_time.tv_usec);
+
+    int buffer_size = 50;
+    unsigned char buffer[buffer_size];
+    uart_read(&uart, buffer, buffer_size);
+    am_util_stdio_printf("Received from serial: %s\r\n", buffer);
+}
