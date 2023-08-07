8ac4d0d7fc37dd962df59b99e1579506d60ee60e
Kristin Ebuengan <ebuengankris@gmail.com>
2023-08-07 16:48:49 -0700

    finished test-time-server

--- src/test-time-server.py
+++ src/test-time-server.py
@@ -1,6 +1,3 @@
-
-
-# from datetime import datetime
 import time
 import serial
 
@@ -8,8 +5,7 @@ import serial
 write program on server to call redboard_rtc commands to do time sync
 """
 
-
-INTERVAL = 0.05  # in seconds
+INTERVAL = 10  # in seconds
 DELAY = 0.05  # in seconds
 
 def constant_time(port):
@@ -23,23 +19,9 @@ def constant_time(port):
     while now < endTime:   # runs for the specified interval
         currTime = time.time()
         currSeconds = int(currTime)
-        currHundredths = int((currTime - currSeconds) * 100)
+        currHundredths = int((currTime - currSeconds) * 1000000)
 
-
-        # currTime = datetime.utcnow()
-        # currWeekday = time.gmtime().tm_wday
-        # currMillisec = int(currTime.microsecond/10000)
-        # currTuple = (
-        #     currTime.year,
-        #     currTime.month,
-        #     currTime.day,
-        #     currWeekday,
-        #     currTime.hour,
-        #     currTime.minute,
-        #     currTime.second,
-        #     currMillisec
-        # )
-        toWrite = bytearray(f"{currSeconds} {currHundredths}\n", 'utf-8')
+        toWrite = bytearray(f"set_time {currSeconds} {currHundredths}\r\n", 'utf-8')
         ser.write(toWrite)
 
         time.sleep(DELAY)
